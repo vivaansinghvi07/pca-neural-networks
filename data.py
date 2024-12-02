7915639115b567b67bc8dd71082bb862d61cc143
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-12-02 15:37:41 -0500

    added normalization to PCA transform

--- data.py
+++ data.py
@@ -7,31 +7,61 @@ import torchvision
 from torch.utils.data import DataLoader
 
 
+def cache(func: typing.Callable):
+    d = {}
+    @wraps(func)
+    def inner(*args, **kwargs):
+        key = args + tuple(kwargs.items())
+        if key not in d:
+            d[key] = func(*args, **kwargs)
+        return d[key]
+    return inner
+
+
 class PCA:
 
-    keep_features: np.ndarray
-    _cum_variance_explained: np.ndarray
-    _components: np.ndarray
+    __keep_features: np.ndarray
+    __base_data: np.ndarray
+    __cum_variance_explained: np.ndarray
+    __components: np.ndarray
 
     def __init__(self, data: np.ndarray):
-        self.keep_features = data.sum(axis=0) != 0
-        nonzero_features = data[:, self.keep_features]
+
+        self.__keep_features = data.sum(axis=0) != 0
+        self.__base_data = data[:, self.__keep_features]
         standardized_features = (
-            nonzero_features - nonzero_features.mean(axis=0)
-        ) / nonzero_features.std(axis=0)
+            self.__base_data - self.__base_data.mean(axis=0)
+        ) / self.__base_data.std(axis=0)
         cov_matrix = np.cov(standardized_features.T)
         evalues, evectors = np.linalg.eig(cov_matrix)
-        self._components = evectors
-        self.cum_variance_explained = np.cumsum(evalues / sum(evalues))
+        self.__components = evectors
+        self.__cum_variance_explained = np.cumsum(evalues / sum(evalues))
+        self.__hash = id(self.__components)  # arbitrary, just needs to be different per object
 
-    def get_projection_matrix(self, thresh: float = 0.95):
+    @cache
+    def _get_projection_matrix(self, thresh: float = 0.95):
         n_components = (
-            np.argwhere(self.cum_variance_explained >= thresh)[0][0] + 1
+            np.argwhere(self.__cum_variance_explained >= thresh)[0][0] + 1
         )  # first instance of being over
-        components = self._components[:, :n_components]
-        return torch.Tensor(
-            components @ np.linalg.inv(components.T @ components) @ components.T
-        )
+        components = self.__components[:, :n_components]
+        return components @ np.linalg.inv(components.T @ components) @ components.T
+
+    @cache
+    def _compute_normalization(
+        self, thresh: float
+    ) -> typing.Tuple[float, float]:
+        projected_data = self._get_projection_matrix(thresh) @ self.__base_data.T
+        return projected_data.mean(), projected_data.std()
+
+    def __call__(self, x: torch.Tensor, thresh: float = 0.95):
+        mat = self._get_projection_matrix(thresh)
+        mean, std = self._compute_normalization(thresh)
+        kept_features = x[self.__keep_features]
+        return (torch.Tensor(mat) @ kept_features - mean) / std
+
+    # for caching to work
+    def __hash__(self) -> int:
+        return self.__hash
 
 
 class PCATransform:
@@ -42,23 +72,8 @@ class PCATransform:
         self.thresh = thresh
         self.pca = precompute_mnist_params()["pca"]
 
-    def __call__(self, x: torch.Tensor) -> torch.Tensor: 
-        kept_features = x[self.pca.keep_features]
-        return self.pca.get_projection_matrix(self.thresh) @ kept_features
-
-
-def cache(func: typing.Callable):
-    d = {}
-
-    @wraps(func)
-    def inner(*args, **kwargs):
-        key = args + tuple(kwargs.items())
-        if key not in d:
-            d[key] = func(*args, **kwargs)
-        return d[key]
-
-    return inner
-
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return self.pca(x, thresh=self.thresh)
 
 @cache
 def precompute_mnist_params():
